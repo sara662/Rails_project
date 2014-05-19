7ba1261c69b703191c0b01879282cecbe7a85a12
sara <sara@sara-Inspiron-N4030.(none)>
2014-05-19 18:57:38 +0200

    3amlt el model we kol el tables el matloba lel Hotel we kaman el relations :D bas lesa mish kamla

--- Hotel/app/models/branch.rb
+++ Hotel/app/models/branch.rb
@@ -1,3 +1,5 @@
 class Branch < ActiveRecord::Base
+	belongs_to :myhotel
 	has_many :rooms
+	
 end
--- Hotel/app/models/roomreserve.rb
+++ Hotel/app/models/roomreserve.rb
@@ -1,2 +1,5 @@
 class Roomreserve < ActiveRecord::Base
+	belongs_to :branch
+	belongs_to :room
+	belongs_to :user
 end
